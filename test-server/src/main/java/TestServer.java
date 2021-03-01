05d67d3b56e3b88f36cdda886f02d5f0561b2ade
zhidian001 <2020104240@ruc.edu.cn>
2021-03-01 22:31:16 +0800

    MyDubbo的一个最简单实现2

--- test-server/src/main/java/TestServer.java
+++ test-server/src/main/java/TestServer.java
@@ -1,9 +1,13 @@
+import register.DefaultServiceRegistry;
+import register.ServiceRegistry;
 import server.RpcServer;
 
 public class TestServer {
     public static void main(String[] args) {
         HelloService helloService = new HelloServiceImpl();
-        RpcServer rpcServer = new RpcServer();
-        rpcServer.register(helloService, 9000);
+        ServiceRegistry serviceRegistry = new DefaultServiceRegistry();
+        serviceRegistry.register(helloService);
+        RpcServer rpcServer = new RpcServer(serviceRegistry);
+        rpcServer.start(9000);
     }
 }
